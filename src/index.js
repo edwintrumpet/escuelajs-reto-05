14a49b34da99792afacd0fc7c77bb8fbd08f87d5
Edwin García <spark.com.co@gmail.com>
2019-09-28 19:22:23 -0500

    Reto 5 terminado

--- src/index.js
+++ src/index.js
@@ -2,25 +2,49 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
+const getData = async api => {
+  let url, output, newItem;
+  const storage = localStorage.getItem('next_fetch');
+
+  if(storage){
+    url = storage;
+  }else{
+    url = api;
+  }
+
+  try {
+    if(url!=='end'){
+      const res = await fetch(url);
+      const response = await res.json();
+      if(storage && storage.substr(-2)==='25'){
+        localStorage.setItem('next_fetch', 'end');
+      }else {
+        localStorage.setItem('next_fetch', response.info.next);
+      }
+
       const characters = response.results;
-      let output = characters.map(character => {
+      output = characters.map(character => {
         return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
+        <article class="Card">
+          <img src="${character.image}" />
+          <h2>${character.name}<span>${character.species}</span></h2>
+        </article>
+        `
       }).join('');
-      let newItem = document.createElement('section');
+      newItem = document.createElement('section');
       newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+    }else {
+      intersectionObserver.unobserve($observe)
+      output = 'Ya no hay personajes…'
+      newItem = document.createElement('p');
+      newItem.innerHTML = output;
+      $app.appendChild(newItem);
+    }
+  }catch(e) {
+    console.log(e)
+  }
 }
 
 const loadData = () => {
@@ -35,4 +59,9 @@ const intersectionObserver = new IntersectionObserver(entries => {
   rootMargin: '0px 0px 100% 0px',
 });
 
+const cleanStorage = () => {
+  localStorage.removeItem('next_fetch');
+}
+
 intersectionObserver.observe($observe);
+window.addEventListener('unload', cleanStorage);
